3ae02df465d0fd9aa3b356a481d44e13119f4500
victorliu-sq <jiaxin-liu@DESKTOP-76UDF3C.localdomain>
2022-12-05 01:09:55 -0500

    v1_1kprobe

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 base = "http://127.0.0.1:5000/"
 
--- worker/worker_server.py
+++ worker/worker_server.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import json
+# it will run this file automatically
+# import jx_cpu_kprobe
+from subprocess import call
+from threading import Thread
+import sys
 
 app = Flask(__name__)
 api = Api(app)
+
+class UserAgent():
+    def run_cpu_kprobe(self):
+        # set pid to 3327 and interval to 1
+        call(["python3", sys.path[0]+"/"+"jx_cpu_kprobe.py"])
+    
+    def run_web_server(self):
+        app.run(debug=True)
 
 class Metrics(Resource):
     def get(self, req_metrics):
@@ -19,4 +32,12 @@ class Metrics(Resource):
 api.add_resource(Metrics, '/<string:req_metrics>')
 
 if __name__ == '__main__':
+    # exec(open('jx_cpu_kprobe').read())
+    UserAgentClass = UserAgent()
+    # thread_web_server = Thread(target=UserAgentClass.run_web_server)
+    thread_cpu_collector = Thread(target=UserAgentClass.run_cpu_kprobe)
+    # thread_web_server.start()
+    thread_cpu_collector.start()
     app.run(debug=True)
+    thread_cpu_collector.join()
+    # thread_web_server.join()
